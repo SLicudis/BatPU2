1da2dc7bbcd44bcf77bf52ff5c770123fa622b6c
Slicudis <140275002+SLicudis@users.noreply.github.com>
2024-11-23 00:16:14 -0300

    Add files via upload

--- BatPU2/projects/quartus/icache.sv
+++ BatPU2/projects/quartus/icache.sv
@@ -7,11 +7,14 @@ module icache( //64 bytes, 4x8, 16-bit
     output busy, mreq
 );
     reg [15:0] cache [31:0];
-    reg [5:0] tags [3:0]; //Valid bit: 5
+    reg [5:0] tags [3:0]; //Valid bit: 4
+    reg [9:0] address_buffer = 0;
     reg busy_state = 0;
     wire miss = (tags[index] != {1'b1, address_in[9:5]}) && req; //Detect for cache miss if tags aren't matching.
 
+
     wire [1:0] index = address_in[4:3]; //Tag index section of the reading address
+    wire [1:0] buffer_idx = address_buffer[4:3];
 
     reg [2:0] offs_pointer = 0; //Points at the lower 4 bits of the address (For line offsets in fetching)
     reg end_phase = 0; //Wait state 2
@@ -19,8 +22,8 @@ module icache( //64 bytes, 4x8, 16-bit
     reg [15:0] out_reg = 0; //Output register for BRAM
 
     assign mreq = (busy_state || miss) && clk_en; //Memory requests are send when a miss happens
-    assign busy = (busy_state || miss) && clk_en; //The core must still output req to the cache, even tho it's stalled.
-    assign address_out = busy_state ? ({address_in[9:3], offs_pointer}+1) : {address_in[9:3], offs_pointer}; //Address for fetching
+    assign busy = busy_state && clk_en; //The core must still output req to the cache, even tho it's stalled.
+    assign address_out = busy_state ? ({address_buffer[9:3], offs_pointer}+1) : {address_buffer[9:3], offs_pointer}; //Address for fetching
     assign inst_out = out_reg;
 
     always_ff @(posedge clk) begin : StateMachine
@@ -30,10 +33,11 @@ module icache( //64 bytes, 4x8, 16-bit
                 busy_state <= miss; //Set busy state if miss
                 end_phase <= 0; //Reset (just in case)
                 offs_pointer <= 0; //Reset (just in case)
+                address_buffer <= address_in;
             end else if(!end_phase) begin //Fetch phases
-                tags[index] <= {1'b1, address_in[9:5]}; //Update the tag
+                tags[buffer_idx] <= {1'b1, address_buffer[9:5]}; //Update the tag
                 offs_pointer <= offs_pointer + 1; //Increment offset pointer for the next word
-                cache[{index, offs_pointer}] <= from_mem; //Write the input word to cache
+                cache[{buffer_idx, offs_pointer}] <= from_mem; //Write the input word to cache
                 if(offs_pointer == 7) end_phase <= 1; //Start ending phase
             end else if(end_phase) begin   //Ending phase of the cache
                 busy_state <= 0; //Reset busy state
